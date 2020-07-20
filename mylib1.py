a547a7dca3107c8ddc67d682dbe6d80db584aede
Jitu Shinde <shindejitu77@gmail.com>
2020-07-20 16:49:42 +0530

    added: Python code.

---
+++ mylib1.py
@@ -0,0 +1,11 @@
+def interest(p, r, t):
+    return (p*r*t)/100
+
+def compoundinterest(p, r, t):
+    c = p
+    for i in range(0, t):
+        c = c + c*r/100
+    return c - p
+
+if __name__ == "__main__":
+    print ("Main..")
